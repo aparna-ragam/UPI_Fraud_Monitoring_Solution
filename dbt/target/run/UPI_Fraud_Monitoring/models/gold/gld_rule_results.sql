7aa9beb6df1caf2980ba3a95244548dcf403f3d0
aparna-ragam <aparna.ragam@tcs.com>
2026-09-07 21:04:59 +0530

    Updates

--- dbt/target/run/UPI_Fraud_Monitoring/models/gold/gld_rule_results.sql
+++ dbt/target/run/UPI_Fraud_Monitoring/models/gold/gld_rule_results.sql
@@ -1,4 +1,11 @@
-SELECT UUID_STRING() AS ALERT_ID,
+
+  create or replace   view UPI_FRAUD_MONITORING_DB.TRANSFORM.gld_rule_results
+  
+  
+  
+  
+  as (
+    SELECT UUID_STRING() AS ALERT_ID,
        result_set.fraud_code,
        f.fraud_name,
        result_set.transaction_id,
@@ -10,7 +17,7 @@ SELECT UUID_STRING() AS ALERT_ID,
        current_timestamp() as load_ts
 from (
     select transaction_id, customer_id, account_id, 'FR001' as fraud_code, 90 as risk_score, 'HIGH_VALUE_UPI' as alert_type
-    from {{ ref('sl_transaction') }}
+    from UPI_FRAUD_MONITORING_DB.TRANSFORM.sl_transaction
     where is_current='TRUE' and txn_amount > 100000
 
     union all
@@ -19,24 +26,26 @@ from (
     from (
         SELECT transaction_id, customer_id, account_id,
                count(*) OVER (PARTITION BY CUSTOMER_ID ORDER BY TXN_DATETIME RANGE BETWEEN INTERVAL '5 MINUTE' PRECEDING AND CURRENT ROW) AS CNT
-        FROM {{ ref('sl_transaction') }} WHERE IS_CURRENT='TRUE'
+        FROM UPI_FRAUD_MONITORING_DB.TRANSFORM.sl_transaction WHERE IS_CURRENT='TRUE'
     )
     WHERE CNT > 10
 
     UNION ALL
 
     SELECT T.TRANSACTION_ID, T.CUSTOMER_ID, T.ACCOUNT_ID, 'FR005' as fraud_code, 80 as risk_score, 'NEW_DEVICE' as alert_type
-    FROM {{ ref('sl_transaction') }} T
-    JOIN {{ ref('sl_device_registry') }} D ON T.DEVICE_ID = D.DEVICE_ID
+    FROM UPI_FRAUD_MONITORING_DB.TRANSFORM.sl_transaction T
+    JOIN UPI_FRAUD_MONITORING_DB.TRANSFORM.sl_device_registry D ON T.DEVICE_ID = D.DEVICE_ID
     WHERE T.IS_CURRENT='TRUE' AND D.IS_CURRENT='TRUE' AND D.TRUSTED_FLAG = 'N'
 
     UNION ALL
 
     SELECT T.TRANSACTION_ID, T.CUSTOMER_ID, T.ACCOUNT_ID, 'FR007' as fraud_code, 100 as risk_score, 'BLACKLISTED_BENEFICIARY' as alert_type
-    FROM {{ ref('sl_transaction') }} T
-    JOIN {{ ref('sl_beneficiary') }} B ON T.BENEFICIARY_ID = B.BENEFICIARY_ID
-    JOIN {{ ref('sl_watchlist') }} W ON B.BENEFICIARY_NAME = W.ENTITY_NAME
+    FROM UPI_FRAUD_MONITORING_DB.TRANSFORM.sl_transaction T
+    JOIN UPI_FRAUD_MONITORING_DB.TRANSFORM.sl_beneficiary B ON T.BENEFICIARY_ID = B.BENEFICIARY_ID
+    JOIN UPI_FRAUD_MONITORING_DB.TRANSFORM.sl_watchlist W ON B.BENEFICIARY_NAME = W.ENTITY_NAME
     WHERE T.IS_CURRENT='TRUE' AND B.IS_CURRENT='TRUE' AND W.IS_CURRENT='TRUE'
 ) result_set
 LEFT JOIN dim_fraud_rule f
   ON result_set.FRAUD_CODE = f.FRAUD_CODE
+  );
+
