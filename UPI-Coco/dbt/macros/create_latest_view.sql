e465e8c0859e46925de408f9becca686126b3be9
aparna-ragam <aparna.ragam@tcs.com>
2026-09-16 13:33:00 +0530

    Update create_latest_view.sql

--- UPI-Coco/dbt/macros/create_latest_view.sql
+++ UPI-Coco/dbt/macros/create_latest_view.sql
@@ -1,4 +1,4 @@
-{% macro create_latest_view(raw_view_ref, delta_column) %}
+{% macro create_latest_view(raw_view_ref, delta_column, primary_key=none) %}
 
     {% set relation = ref(raw_view_ref) %}
     {% set columns = adapter.get_columns_in_relation(relation) %}
@@ -25,18 +25,45 @@
     {% endfor %}
 
     with cleansed as (
-        select distinct
+        select
             {{ cleansed_columns | join(',\n            ') }},
             md5({{ concat_expr | join(" || '|' || ") }}) as hash_diff
         from {{ relation }}
         where {{ delta_column }} is not null
+    ),
+
+    latest_batch as (
+        select *
+        from cleansed
+        where {{ delta_column }} = (
+            select max({{ delta_column }})
+            from cleansed
+        )
     )
 
-    select *
-    from cleansed
-    where {{ delta_column }} = (
-        select max({{ delta_column }})
-        from cleansed
+    {% if primary_key is not none %}
+    , deduped as (
+        select *,
+            row_number() over (
+                partition by {{ primary_key }}
+                order by {{ delta_column }} desc, hash_diff
+            ) as rn
+        from latest_batch
     )
+
+    select
+        {% for col in columns %}
+        {{ col.name }},
+        {% endfor %}
+        hash_diff
+    from deduped
+    where rn = 1
+
+    {% else %}
+
+    select distinct *
+    from latest_batch
+
+    {% endif %}
 
 {% endmacro %}
